bfa30848b598f00f358c8fc1e537327de743b0a1
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-06-19 13:22:48 -0500

    desctruturar datos con array

--- src/topics/05-basic-destructuting.ts
+++ src/topics/05-basic-destructuting.ts
@@ -29,4 +29,10 @@ console.log(volume, duration, title, details);
 const { artist, album, genre } = details;
 console.log(artist, album, genre);
 
+// Destructuring with array
+const dbz:string[] = ['Goku', 'Vegeta', 'Gohan', 'Piccolo'];
+console.log(dbz[0], dbz[1], dbz[2], dbz[3]);
+const [hero1, hero2, hero3, hero4] = dbz;
+console.log(hero1, hero2, hero3, hero4);
+
 export {};
